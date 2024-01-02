6b793aeba214bb1d1f5c10ea5485c4b212ed7357
Tyan <sebastians2896@gmail.com>
2024-01-02 08:43:38 -0500

    Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>Counter</h2>
+    <p>{{ counter }} <sup>2</sup> =  {{squareCounter}}</p>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+
+        data(){
+
+            return{
+                counter: 5
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+            squareCounter() {
+                console.log('computed squareCounter')
+                return this.counter * this.counter
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
